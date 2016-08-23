e62c82c59ca1ad5e6338984fa54edcdc1a7d7697
“Juan” <jmarrugo79@gmail.com>
2016-08-22 22:31:44 -0500

    Proyecto Finalizado

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -58,46 +58,62 @@ public class Principal extends javax.swing.JFrame {
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 20, 280, -1));
 
         jLabel2.setText("Inversion 1:");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, -1, -1));
 
         jLabel3.setText("Inversion 2:");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 70, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 70, -1, -1));
 
         jLabel4.setText("Inversion 3:");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 70, -1, -1));
-        getContentPane().add(txtInversionUno, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 70, 60, -1));
+
+        txtInversionUno.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtInversionUnoKeyTyped(evt);
+            }
+        });
+        getContentPane().add(txtInversionUno, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 70, 60, -1));
 
         txtInversionDos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtInversionDosActionPerformed(evt);
             }
         });
-        getContentPane().add(txtInversionDos, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 70, 60, -1));
+        txtInversionDos.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtInversionDosKeyTyped(evt);
+            }
+        });
+        getContentPane().add(txtInversionDos, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 70, 60, -1));
 
         txtInversionTres.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtInversionTresActionPerformed(evt);
             }
         });
+        txtInversionTres.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtInversionTresKeyTyped(evt);
+            }
+        });
         getContentPane().add(txtInversionTres, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 70, 60, -1));
 
         jLabel5.setText("%");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 110, -1, -1));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, -1, -1));
 
         txtPorcentajeUno.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtPorcentajeUnoActionPerformed(evt);
             }
         });
-        getContentPane().add(txtPorcentajeUno, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 110, 60, -1));
-        getContentPane().add(txtPorcentajeDos, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 110, 60, -1));
-        getContentPane().add(txtPorcentajeTres, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 110, 60, -1));
+        getContentPane().add(txtPorcentajeUno, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 110, 90, -1));
+        getContentPane().add(txtPorcentajeDos, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 110, 90, -1));
+        getContentPane().add(txtPorcentajeTres, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 110, 90, -1));
 
         jLabel6.setText("%");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 110, -1, -1));
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 110, -1, -1));
 
         jLabel7.setText("%");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 110, -1, -1));
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 110, -1, -1));
 
         jLabel8.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel8.setText("Inversion Total :");
@@ -119,6 +135,11 @@ public class Principal extends javax.swing.JFrame {
         getContentPane().add(cmdCalcular, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 260, -1, -1));
 
         cmdBorrar.setText("Borrar");
+        cmdBorrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdBorrarActionPerformed(evt);
+            }
+        });
         getContentPane().add(cmdBorrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 260, -1, -1));
 
         pack();
@@ -141,8 +162,116 @@ public class Principal extends javax.swing.JFrame {
     }//GEN-LAST:event_txtInversionTotalActionPerformed
 
     private void cmdCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdCalcularActionPerformed
+       double suma;
+       int persona1, persona2, persona3;
+       double porc1, porc2, porc3;
+       String res1, res2, res3, total;
+     
+
+    if(txtInversionUno.getText().trim().isEmpty()){
+         JOptionPane.showMessageDialog(this, "Digite la Inversion número Uno","error", JOptionPane.ERROR_MESSAGE);
+         txtInversionUno.requestFocusInWindow();
+         txtInversionUno.selectAll();
+//validación de vacio de inversion 1
+       }
+
+     else if(txtInversionDos.getText().trim().isEmpty()){
+        JOptionPane.showMessageDialog(this, "Digite la Inversion número Dos","error", JOptionPane.ERROR_MESSAGE);  
+        txtInversionDos.requestFocusInWindow();
+        txtInversionDos.selectAll();
+//validación de vacio de inversion 2
+     }  
+     else if(txtInversionTres.getText().trim().isEmpty()){
+        JOptionPane.showMessageDialog(this, "Digite la Inversion número tres","error", JOptionPane.ERROR_MESSAGE);  
+        txtInversionTres.requestFocusInWindow(); 
+         txtInversionTres.selectAll();    
+////validación de vacio de inversion 3
+     }
+    
+     
+     else{
+  
+     persona1=Integer.parseInt(txtInversionUno.getText());
+     persona2=Integer.parseInt(txtInversionDos.getText());
+     persona3=Integer.parseInt(txtInversionTres.getText());
+ // convertir los Labels en enteros     
+ 
+   if(persona1==persona2) {
+       JOptionPane.showMessageDialog(this, "Las cantidades individuales deben ser diferentes","error", JOptionPane.ERROR_MESSAGE);  
+        txtInversionUno.requestFocusInWindow(); 
+        txtInversionUno.selectAll();  
+       }  
+
+     else if(persona2==persona3){
+     JOptionPane.showMessageDialog(this, "Las cantidades individuales deben ser diferentes","error", JOptionPane.ERROR_MESSAGE);  
+        txtInversionDos.requestFocusInWindow(); 
+        txtInversionDos.selectAll();              
+       }      
+ else if(persona1==persona3){
+     JOptionPane.showMessageDialog(this, "Las cantidades individuales deben ser diferentes","error", JOptionPane.ERROR_MESSAGE);  
+        txtInversionUno.requestFocusInWindow(); 
+        txtInversionUno.selectAll();              
+       }     
+suma=(persona1+persona2+persona3); 
+   porc1=(persona1/suma)*100;
+   porc2=(persona2/suma)*100;
+   porc3=(persona3/suma)*100;
+
+ //Iniciar Operacion
+     res1=String.valueOf(porc1);
+    txtPorcentajeUno.setText (res1);    
+    
+    res2=String.valueOf(porc2);
+    txtPorcentajeDos.setText (res2);  
+    
+    res3=String.valueOf(porc3);
+    txtPorcentajeTres.setText(res3); 
+
+    total=String.valueOf(suma);
+    txtInversionTotal.setText(total);
+  
+   }
        
     }//GEN-LAST:event_cmdCalcularActionPerformed
+
+    private void cmdBorrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdBorrarActionPerformed
+        txtInversionTotal.setText("");
+     txtInversionUno.setText("");
+     txtInversionDos.setText("");
+     txtInversionTres.setText("");
+     txtPorcentajeUno.setText("");
+     txtPorcentajeDos.setText("");
+     txtPorcentajeTres.setText("");
+     txtInversionTotal.requestFocusInWindow();
+     txtInversionUno.requestFocusInWindow();
+    }//GEN-LAST:event_cmdBorrarActionPerformed
+
+    private void txtInversionUnoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtInversionUnoKeyTyped
+        char c=evt.getKeyChar(); 
+       
+          if(!Character.isDigit(c)) { 
+              getToolkit().beep();    
+              evt.consume(); 
+          }  
+    }//GEN-LAST:event_txtInversionUnoKeyTyped
+
+    private void txtInversionDosKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtInversionDosKeyTyped
+        char c=evt.getKeyChar(); 
+       
+          if(!Character.isDigit(c)) { 
+              getToolkit().beep();    
+              evt.consume(); 
+          }  
+    }//GEN-LAST:event_txtInversionDosKeyTyped
+
+    private void txtInversionTresKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtInversionTresKeyTyped
+       char c=evt.getKeyChar(); 
+       
+          if(!Character.isDigit(c)) { 
+              getToolkit().beep();    
+              evt.consume(); 
+          }  
+    }//GEN-LAST:event_txtInversionTresKeyTyped
 
     /**
      * @param args the command line arguments
